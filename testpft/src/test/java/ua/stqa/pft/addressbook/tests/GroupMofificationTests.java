e351c107a00cf4eeb9bcb8ac02f07df4f8e4a5bc
Yurii Voina <yurii.voina@accenture.com>
2018-06-12 09:49:34 +0300

    Added getGroupCount method

--- testpft/src/test/java/ua/stqa/pft/addressbook/tests/GroupMofificationTests.java
+++ testpft/src/test/java/ua/stqa/pft/addressbook/tests/GroupMofificationTests.java
@@ -13,7 +13,7 @@ public class GroupMofificationTests extends TestBase {
         if(! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().selectGroup(before - 1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test2", "test2", "test4"));
         app.getGroupHelper().submitGroupModification();
